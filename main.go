3e320792e6d174eb333f0ad6f858960a6bd98558
Iratsareva <irinatsarea1211@gmail.com>
2025-12-09 19:16:56 +0300

    fix 9

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
     "log"
     "net/http"
+    "sync"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -10,6 +11,28 @@ func Handler(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Hello from Service 1!\n"))
 }
 
+func Handler2(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("Hello from Service 2!\n"))
+}
+
 func main() {
-    log.Fatal(http.ListenAndServe(":8080", http.HandlerFunc(Handler)))
+    var wg sync.WaitGroup
+    wg.Add(2)
+    
+    // Сервис 1 на порту 8080
+    go func() {
+        defer wg.Done()
+        log.Println("Запуск Service 1 на порту :8080")
+        log.Fatal(http.ListenAndServe(":8080", http.HandlerFunc(Handler)))
+    }()
+    
+    // Сервис 2 на порту 8081
+    go func() {
+        defer wg.Done()
+        log.Println("Запуск Service 2 на порту :8081")
+        log.Fatal(http.ListenAndServe(":8081", http.HandlerFunc(Handler2)))
+    }()
+    
+    wg.Wait()
 }
